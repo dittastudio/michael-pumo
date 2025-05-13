ccb6f9c9a9c900a50c04aa5a6c8e189221bd59ce
Michael Pumo <hello@michaelpumo.com>
2025-05-13 22:35:51 +0100

    Basic app footer.

--- app.vue
+++ app.vue
@@ -39,6 +39,8 @@ useMenu(false)
   <div>
     <NuxtPage />
 
+    <AppFooter />
+
     <DevGuide />
   </div>
 </template>
---
+++ components/app/Footer.vue
@@ -0,0 +1,42 @@
+<script lang="ts" setup>
+import Ditta from '@/assets/logos/ditta.svg'
+</script>
+
+<template>
+  <footer class="w-full px-gutter py-20">
+    <div class="w-full flex flex-col gap-gutter-md">
+      <div class="w-full flex flex-col gap-7 max-w-[50ch] text-balance">
+        <p class="text-26 text-tertiary">
+          It’s time to build your next web project.
+          Let’s work together and deliver success.
+        </p>
+
+        <p class="text-26 text-tertiary">
+          <a href="mailto:hello@michaelpumo.com">
+            hello@michaelpumo.com
+          </a>
+        </p>
+      </div>
+
+      <div class="w-full flex flex-col gap-4">
+        <p>
+          <a
+            href="https://ditta.studio"
+            target="_blank"
+            class="inline-block text-tertiary"
+          >
+            <Ditta class="w-15 h-auto" />
+
+            <span class="sr-only">
+              ditta
+            </span>
+          </a>
+        </p>
+
+        <p class="text-12 max-w-[40ch] text-balance">
+          ditta ltd. is a company registered in England and Wales with company number 15648076.
+        </p>
+      </div>
+    </div>
+  </footer>
+</template>
