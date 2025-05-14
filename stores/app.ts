f7f6d20f34de2896c5941338804b238b860317af
Michael Pumo <hello@michaelpumo.com>
2025-05-14 14:44:07 +0100

    Fix persistance issue.

--- stores/app.ts
+++ stores/app.ts
@@ -1,37 +1,35 @@
 import { defineStore } from 'pinia'
 
-export const useAppStore = defineStore('app', () => {
-  const menu = ref(false)
-  const theme = ref<App.Theme>('dark')
-  const typeface = ref<App.Typeface>('saans')
+export const useAppStore = defineStore(
+  'app',
+  () => {
+    const theme = ref<App.Theme>('dark')
+    const typeface = ref<App.Typeface>('saans')
 
-  const getMenu = computed(() => menu.value)
-  const getTheme = computed(() => theme.value)
-  const getTypeface = computed(() => typeface.value)
+    const getTheme = computed(() => theme.value)
+    const getTypeface = computed(() => typeface.value)
 
-  function setMenu(value: boolean) {
-    menu.value = value
-  }
+    function setTheme(value: App.Theme) {
+      theme.value = value
+    }
 
-  function setTheme(value: App.Theme) {
-    theme.value = value
-  }
+    function setTypeface(value: App.Typeface) {
+      typeface.value = value
+    }
 
-  function setTypeface(value: App.Typeface) {
-    typeface.value = value
-  }
-
-  return {
-    getMenu,
-    getTheme,
-    getTypeface,
-    setMenu,
-    setTheme,
-    setTypeface,
-  }
-}, {
-  persist: {
-    pick: ['theme'],
-    storage: piniaPluginPersistedstate.cookies(),
+    return {
+      theme,
+      typeface,
+      getTheme,
+      getTypeface,
+      setTheme,
+      setTypeface,
+    }
   },
-})
+  {
+    persist: {
+      pick: ['theme', 'typeface'],
+      storage: piniaPluginPersistedstate.cookies(),
+    },
+  },
+)
