2d0e5facfd95b4ae530584f3a4640832c2529f2d
Michael Pumo <hello@michaelpumo.com>
2026-03-08 20:02:59 +0000

    refactor: improve formatting and error handling in useStory function

--- app/composables/useStory.ts
+++ app/composables/useStory.ts
@@ -1,6 +1,9 @@
 import type { ISbStoriesParams, ISbStoryData, StoryblokBridgeConfigV2 } from '@storyblok/js'
 
-type UseAsyncDataOptions = Omit<Parameters<typeof useAsyncStoryblok>[1], 'api' | 'bridge'>
+type UseAsyncDataOptions = Omit<
+  Parameters<typeof useAsyncStoryblok>[1],
+  'api' | 'bridge'
+>
 
 export async function useStory<T>(
   slug: string = '',
@@ -11,27 +14,33 @@ export async function useStory<T>(
   const runtimeConfig = useRuntimeConfig()
   const route = useRoute()
 
-  const { story, error } = await useAsyncStoryblok(
-    storyblokSlug(slug),
-    {
-      api: {
-        version: runtimeConfig.public.STORYBLOK_VERSION === 'published' ? 'published' : 'draft',
-        from_release: typeof route.query?._storyblok_release === 'string' ? route.query?._storyblok_release : undefined,
-        ...api,
-      },
-      bridge: {
-        resolveLinks: 'url',
-        preventClicks: true,
-        ...bridge,
-      },
-      deep: true,
-      ...options,
+  const { story, error } = await useAsyncStoryblok(storyblokSlug(slug), {
+    api: {
+      version:
+        runtimeConfig.public.STORYBLOK_VERSION === 'published'
+          ? 'published'
+          : 'draft',
+      from_release:
+        typeof route.query?._storyblok_release === 'string'
+          ? route.query?._storyblok_release
+          : undefined,
+      ...api,
     },
-  )
+    bridge: {
+      resolveLinks: 'url',
+      preventClicks: true,
+      ...bridge,
+    },
+    transform: (input) => {
+      return input
+    },
+    deep: true,
+    ...options,
+  })
 
   if (error.value) {
     throw createError({
-      statusCode: error.value.statusCode || 404,
+      statusCode: error.value.status || 404,
       statusMessage: `Page not found${slug ? ` for: ${slug}` : ''}`,
       fatal: true,
       cause: error.value,
